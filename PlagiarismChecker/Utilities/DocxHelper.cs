0271835ae4e36e8df82eaad43339df60ce6fb665
LIU Wei <liuwei@nbu.edu.cn>
2015-07-13 22:23:56 +0800

    实现将 DOC 转换为 DOCX

---
+++ PlagiarismChecker/Utilities/DocxHelper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using DocumentFormat.OpenXml.Drawing.Pictures;
+using DocumentFormat.OpenXml.Wordprocessing;
+using NetOffice.WordApi.Enums;
+using Picture = DocumentFormat.OpenXml.Drawing.Pictures.Picture;
+
+namespace PlagiarismChecker.Utilities
+{
+	public class DocxHelper
+	{
+		public static NetOffice.WordApi.Application GetWordInstance()
+		{
+			return new NetOffice.WordApi.Application();
+		}
+
+		public static void ConvertToDocx(IEnumerable<string> docFiles)
+		{
+			using (var word = new NetOffice.WordApi.Application())
+			{
+				word.DisplayAlerts = WdAlertLevel.wdAlertsNone;
+
+				foreach (var file in docFiles)
+				{
+					var doc = word.Documents.Open(file, null, true);
+					var extName = ".docx";
+
+					string newName = Path.Combine(Path.GetDirectoryName(file), Path.GetFileNameWithoutExtension(file) + extName);
+					doc.SaveAs2(newName, WdSaveFormat.wdFormatXMLDocument, null, null, null, null, null, null, null, null, null, null,
+						null, null, null, null, WdCompatibilityMode.wdCurrent);
+
+					doc.Close();
+				}
+				word.Quit();
+				word.Dispose();
+			}
+		}
+
+		public static void GetAllPictures(NetOffice.WordApi.Application _instance, string file)
+		{
+
+
+
+		}
+	}
+}
--- PlagiarismChecker/frmMain.cs
+++ PlagiarismChecker/frmMain.cs
@@ -3,9 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Text;
 using System.Windows.Forms;
-using PlagiarismCheckerClassLibrary;
+using NetOffice.WordApi;
+using NetOffice.WordApi.Enums;
+using PlagiarismChecker.Utilities;
+using NetOffice;
+
 
 namespace PlagiarismChecker
 {
@@ -18,18 +23,26 @@ namespace PlagiarismChecker
 
 		private void frmMain_Load(object sender, EventArgs e)
 		{
-			
 		}
 
 		private void button1_Click(object sender, EventArgs e)
 		{
+			List<string> docFiles = new List<string>();
 			OpenFileDialog ofdMain = new OpenFileDialog();
+			ofdMain.Filter = "doc文件|*.doc";
+			ofdMain.Multiselect = true;
 
 			if (ofdMain.ShowDialog() == DialogResult.OK)
 			{
-				string filePath = ofdMain.FileName;
+				foreach (var fileName in ofdMain.FileNames)
+				{
+					if (Path.GetExtension(fileName) == ".doc")
+					{
+						docFiles.Add(fileName);
+					}
+				}
 
-				
+				DocxHelper.ConvertToDocx(docFiles.ToArray());
 			}
 		}
 	}
