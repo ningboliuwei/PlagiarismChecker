c374f79eae4abc53a2a300ee31c43d8b80df1180
ningboliuwei <liuwei@nbu.edu.cn>
2015-07-13 13:23:16 +0800

    暂时提交

---
+++ PlagiarismChecker/frmMain.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using PlagiarismCheckerClassLibrary;
+
+namespace PlagiarismChecker
+{
+	public partial class frmMain : Form
+	{
+		public frmMain()
+		{
+			InitializeComponent();
+		}
+
+		private void frmMain_Load(object sender, EventArgs e)
+		{
+			
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog ofdMain = new OpenFileDialog();
+
+			if (ofdMain.ShowDialog() == DialogResult.OK)
+			{
+				string filePath = ofdMain.FileName;
+
+				
+			}
+		}
+	}
+}
